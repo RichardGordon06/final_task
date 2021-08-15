765e564e3b0ea9afaea29183b2319de8ecac7962
RichardGordon06 <87995654+RichardGordon06@users.noreply.github.com>
2021-08-15 15:12:55 +0300

    Add files via upload
    Добавление параметризации для поиска бага, и отметка этого бага с помощью xfail

---
+++ test_product_page.py
@@ -0,0 +1,26 @@
+from .pages.product_page import ProductPage
+from .pages.locators import Locators
+import time
+import pytest
+
+@pytest.mark.parametrize('links', [pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",     
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, links):
+    link = f"{links}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.solve_quiz_and_get_code()
+    browser.implicitly_wait(10)
+    page.assert_message_to_add_basket()
+    page.assert_toward_in_basket()
+    page.assert_price_basket()
+    page.assert_price_in_basket_equal_toward()
